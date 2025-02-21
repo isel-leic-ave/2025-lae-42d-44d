e8655fefbc48ff8da9602c13debe6d5ffe3f3679
Miguel Gamboa <miguelgamboa@outlook.com>
2025-02-21 17:06:30 +0000

    lesson02 - reflect intro

---
+++ lesson02-reflect-javascript/index.js
@@ -0,0 +1,11 @@
+function invokeAllMethods(obj) {
+    Object
+        .keys(obj.constructor.prototype)
+        .map(name => obj[name])
+        .filter(member => typeof member === 'function' && member.length === 0)
+        .forEach(fn => console.log(fn.name + "() => " + fn.call(obj)))
+}
+
+// invokeAllMethods(new URL("http://isel.pt"))
+
+invokeAllMethods(performance)
