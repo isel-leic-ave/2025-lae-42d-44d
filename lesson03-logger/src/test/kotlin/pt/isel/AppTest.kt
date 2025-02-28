4f77941b33db743f38d06b2d1100c821479bf79c
Miguel Gamboa <miguelgamboa@outlook.com>
2025-02-28 15:33:10 +0000

    Clean tests

--- lesson03-logger/src/test/kotlin/pt/isel/AppTest.kt
+++ lesson03-logger/src/test/kotlin/pt/isel/AppTest.kt
@@ -5,27 +5,33 @@ import kotlin.test.Test
 import kotlin.test.assertNotNull
 
 class AppTest {
-    @Test fun `test Logger with an Artist`() {
+    @Test
+    fun `test Logger with an Artist`() {
         val expected = """Artist
- - country = Country
-   - idiom = en
-   - name = UK
- - kind = Rock
- - name = David Bowie
-"""
+            - country = Country
+              - idiom = en
+              - name = UK
+            - kind = Rock
+            - name = David Bowie
+            """.lines().iterator()
         val bowie = Artist("Rock", "David Bowie", Country("UK", "en"))
         StringBuilder()
             .also {
                 it.log(bowie)
-                assertEquals(expected, it.toString())
+                it.toString().lines().forEach { actual ->
+                    assertEquals(expected.next().trim(), actual.trim())
+                }
             }
     }
-    @Test fun `test Logger with an Rectangle`() {
+
+    @Test
+    fun `test Logger with an Rectangle`() {
         val rect = Rectangle(5, 7)
         System.out.log(rect)
     }
 
-    @Test fun `test Logger with an JavaRectangle`() {
+    @Test
+    fun `test Logger with an JavaRectangle`() {
         val rect = JavaRectangle(5, 7)
         System.out.log(rect)
     }
